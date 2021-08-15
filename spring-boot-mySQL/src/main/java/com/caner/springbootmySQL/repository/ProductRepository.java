1cc6ff820a7cf0d6256ffc19893b6f905c63f5f9
caner <canerturkmen@msn.com>
2021-08-15 03:35:21 +0300

    product repository added

---
+++ spring-boot-mySQL/src/main/java/com/caner/springbootmySQL/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.caner.springbootmySQL.repository;
+
+import com.caner.springbootmySQL.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
